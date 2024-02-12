34cbc695d4d28cead3d7521f7eef576829fc629e
AmbitiousRabbit <elisemathis3@gmail.com>
2024-02-11 21:10:37 -0600

    Added other libraries and testing out the visuals above the checkbox function

--- Web_App.py
+++ Web_App.py
@@ -1,11 +1,20 @@
-import streamlit as st
-import plotly.express as px
 import pandas as pd
+import streamlit as st
+import numpy as np
+import plotly.express as px
+import altair as alt
+
 
 df = pd.read_csv("spotify-2023.csv", encoding='latin1')
 
 st.header('Spotify\'s Top Popular Songs For 2023', divider='rainbow')          
 st.header(':green[My Software Dev Project] - Sprint 4')
+
+fig = px.histogram(df, x='streams', y='speechiness_%', histfunc='avg')
+fig.show()
+
+fig = px.scatter_matrix(df, dimensions=["energy_%", "bpm", "valence_%"])
+fig.show()
 
 
 #Test Visual 1
@@ -16,8 +25,4 @@ st.write('Did this Visual Display Correctly? Click the checkbox below', check)
 if check:
     st.write(":sunglasses:"*6)
 
-fig = px.histogram(df, x='streams', y='speechiness_%', histfunc='avg')
-fig.show()
 
-fig = px.scatter_matrix(df, dimensions=["energy_%", "bpm", "valence_%"])
-fig.show()
