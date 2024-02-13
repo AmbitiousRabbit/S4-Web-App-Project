0c0b967196c22dce1b7707f5f4dd75f20da483d0
AmbitiousRabbit <elisemathis3@gmail.com>
2024-02-13 15:59:57 -0600

    Made Adjustments to Figures with st.write()

---
+++ Test.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.header('Spotify\'s Top Popular Songs For 2023', divider='rainbow')
--- Web_App.py
+++ Web_App.py
@@ -4,11 +4,10 @@ import numpy as np
 import plotly.express as px
 import altair as alt
 
-
-df = pd.read_csv("spotify-2023.csv", encoding='latin1')
-
 st.header('Spotify\'s Top Popular Songs For 2023', divider='rainbow')          
 st.header(':green[My Software Dev Project] - Sprint 4')
+
+df = pd.read_csv("spotify-2023.csv", encoding='latin1')
 
 #Test Visual 1
 
@@ -20,12 +19,11 @@ if check:
 
 
 fig = px.histogram(df, x='streams', y='speechiness_%', histfunc='avg')
-#st.write('Hello, World!')
-fig.show()
+st.write(fig)
 
 fig = px.scatter_matrix(df, dimensions=["energy_%", "bpm", "valence_%"])
-fig.show()
+st.write(fig)
 
 
-"C:\Hopefully the Final S4 Project Folder\S4-Web-App-Project\Web_App.py"
+st.header('TripleTen Project')
 
